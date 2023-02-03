ff89b0f535121d94830b3649122f3cbb9eba2723
popcorn412 <felix44291@gmail.gom>
2023-02-02 21:53:57 -0800

    init

--- chains/ethereum/index.ts
+++
@@ -1,4 +0,0 @@
-export type { Chain, ChainProviderFn, Connector, Provider, WebSocketProvider } from '@wagmi/core'
-export { Web3ModalEthereum } from './src/api'
-export { chains, providers } from './src/utils/wagmiTools'
-export * from './types/apiTypes'
--- packages/core/src/utils/PolyfillUtil.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-unnecessary-condition */
-import { Buffer } from 'buffer'
-
-if (typeof window !== 'undefined') {
-  if (!window.Buffer) window.Buffer = Buffer
-  if (!window.global) window.global = window
-  // @ts-expect-error minimal process
-  if (!window.process) window.process = { env: {} }
-}
